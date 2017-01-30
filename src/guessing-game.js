0ff6c6ac063d81523d2f6269c20d5c360da48582
VolhaChubakova <oachubakova@gmail.com>
2017-01-30 22:32:03 +0300

    Add files via upload

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,66 +1,55 @@
 class GuessingGame {
 
-
-
-    constructor() {
+   constructor() {
 
     this.number=0;
     this.min=0;
     this.max=0; 
 
-    }
-
-       
+    }      
         setRange(min, max) {
-
             this.min=min;
             this.max=max;                
             }
 
         guess() {
 
-
         const number=this.number;
        	const game = new GuessingGame();
                 	
       	game.setRange(this.min, this.max);
-
-
-        while ( this.min <= this.max) 
-        {
-          
-         let result= (this.min + this.max)/2;
-         
-         if (result == number) { 
-                return number;
-            }
-
-         else if (result < number) {
-            return game.greater();
-         }
         
-         else { 
-          return game.lower();
-         }
+       
 
-         return null;
+        while ( this.min <= this.max) {
 
+            if (Math.ceil(result) == number) { 
+                return number;
+                }
+
+            else if (result < number) {
+                 return game.greater();
+                 }
+        
+            else { 
+                  return game.lower();
+                 }
+          
+             var result= Math.ceil( (this.min + this.max)/2 );
         }
+
+          return null;
       }
       
-
-
     lower() {
-         let result=(this.min+this.max)/2;
-    		 return this.max=result-1;   	 
+        var result= Math.ceil( (this.min + this.max)/2 );
+        return this.max=Math.ceil(result-1);	 
             }
 
     greater() {
-                 let result= (this.min+this.max)/2;
-               return  this.min=result+1; 
-
-
-             }
+        var result= Math.ceil( (this.min + this.max)/2 );
+        return  this.min=Math.ceil(result+1); 
+            }
 
 }
 
