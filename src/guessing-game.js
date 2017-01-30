a4cd7aaf313989c87c7d5adab45bfebff04bd617
VolhaChubakova <oachubakova@gmail.com>
2017-01-30 07:33:50 +0300

    first

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,58 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    this.number=null;
+    }
 
     setRange(min, max) {
 
-    }
+    	while (min==0){
+            var start=0;
+         	  var end=max;
+    	} 
+    	   		
+       }
 
     guess() {
 
+    	const game = new GuessingGame();
+    	game.setRange(min, max);
+
+
+        let middle= (start + end) / 2;
+
+        
+       while (result != number )
+
+        {
+
+        if (result==middle) {
+              result==number;
+              return number;
+            }
+       	if (result<middle){
+       		game.lower();
+       	}
+
+        else if (result>middle){
+       		game.greater();
+       	}
+	
+        return number;
+        }
+      	
     }
 
     lower() {
-
-    }
+    	   start=start; 
+    		end=result-1;   	          
+        }
 
     greater() {
-
+    	start=result+1; 
+    	end=end;
     }
+
 }
 
 module.exports = GuessingGame;
