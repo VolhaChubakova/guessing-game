4cd77726e45647e5d7ba0b343cc675c5e8e23923
VolhaChubakova <oachubakova@gmail.com>
2017-02-05 14:03:30 +0300

    final-version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,52 +1,45 @@
 class GuessingGame {
 
    constructor() {
-
-    this.number=0;
-    this.min=0;
-    this.max=0; 
-
+     this.number=0;
+     this.min=0;
+     this.max=0; 
     }      
-        setRange(min, max) {
-            this.min=min;
-            this.max=max;                
-            }
 
-        guess() {
+    setRange(min, max) {
+      this.min=min;
+      this.max=max;                
+    }
 
-        const number=this.number;
-       	const game = new GuessingGame();
-        game.setRange(this.min, this.max);
+    guess() {
+      const number=this.number;
+      const game = new GuessingGame();
+      game.setRange(this.min, this.max);
         
-        while ( this.min <= this.max) {
-
-            if (result == number) { 
-                return number;
-                }
-
-            else if (result < number) {
-                 return game.greater();
-                 }
-        
-            else { 
-                  return game.lower();
-                 }
-          
-             var result= Math.ceil((this.min + this.max)/2);
-        }
-
-          return null;
+      while ( this.min <= this.max) {
+         if (result == number) { 
+            return number;
+          }
+          else if (result < number) {
+             return game.greater();
+          }
+          else { 
+              return game.lower();
+          }
+      var result= Math.ceil((this.min + this.max)/2);
       }
+      return null;
+    }
       
     lower() {
-        var result= Math.ceil((this.min + this.max)/2);
-        return this.max=result;	 
-            }
+      var result= Math.ceil((this.min + this.max)/2);
+      return this.max=result;	 
+    }
 
     greater() {
-        var result= Math.ceil( (this.min + this.max)/2 );
-        return  this.min=result; 
-            }
+      var result= Math.ceil( (this.min + this.max)/2 );
+      return  this.min=result; 
+    }
 
 }
 
